1abe54de0f37ad8c685a42b4e2a6ccf66796f026
NikitaShvindt <116110595+NikitaShvindt@users.noreply.github.com>
2023-04-17 13:37:04 +0200

    add js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,17 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = galleryItems.map(({preview, original, description}) => 
+        `<li class="gallery__item"><a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a></li>`).join("");
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
 
 console.log(galleryItems);
+
